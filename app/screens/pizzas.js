038a8b17c7e88cfd1f3596f33b8c19cc4b3bf923
Carlos Eduardo Angulo Ustarez <carlos_angulo_az@hotmail.com>
2021-08-15 15:49:39 -0400

    Screen layout to display the pizza list

--- app/screens/pizzas.js
+++ app/screens/pizzas.js
@@ -2,13 +2,54 @@ import * as React from "react";
 import { Button, View, Text } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Icon } from "react-native-elements";
+
+import { ListItem, Avatar } from "react-native-elements";
 
 export default function Pizzas({ navigation }) {
+  const list = [
+    {
+      pizzaid: 1,
+      description: "Hawaiian",
+      subtitle: "Pizza",
+    },
+    {
+      pizzaid: 2,
+      description: "Peperoni",
+      subtitle: "Pizza",
+    },
+    {
+      pizzaid: 3,
+      description: "Irish",
+      subtitle: "Pizza",
+    },
+  ];
+
   return (
     <View>
-      <Text>Pizzas...</Text>
+      {list.map((l, i) => (
+        <ListItem
+          key={l.pizzaid}
+          bottomDivider
+          onPress={() => console.log({ i })}
+        >
+          <Icon name="pizza-slice" type="font-awesome-5" color="#f81" />
+          <ListItem.Content>
+            <ListItem.Title>{l.description}</ListItem.Title>
+            <ListItem.Subtitle>{l.subtitle}</ListItem.Subtitle>
+          </ListItem.Content>
+          <Icon
+            raised
+            name="trash"
+            type="font-awesome-5"
+            color="#f50"
+            onPress={() => console.log("hello")}
+          />
+        </ListItem>
+      ))}
+
       <Button
-        title="Go to Details"
+        title="Add new Pizza"
         onPress={() => navigation.navigate("PizzaDetail")}
       />
     </View>
