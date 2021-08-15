a9f04655d162b4d95e258b28caef4efe02c7042c
Carlos Eduardo Angulo Ustarez <carlos_angulo_az@hotmail.com>
2021-08-14 20:25:28 -0400

    Initializing the project

---
+++ app/screens/pizzadetail.js
@@ -0,0 +1,11 @@
+import {} from "react";
+import { View } from "react-native";
+import { View, Text } from "react-native";
+
+export default function Pizzadetail() {
+  return (
+    <View>
+      <Text>Pizzadetail...</Text>
+    </View>
+  );
+}
---
+++ app/screens/pizzas.js
@@ -0,0 +1,11 @@
+import {} from "react";
+import { View } from "react-native";
+import { View, Text } from "react-native";
+
+export default function Pizzas() {
+  return (
+    <View>
+      <Text>Pizzas...</Text>
+    </View>
+  );
+}
