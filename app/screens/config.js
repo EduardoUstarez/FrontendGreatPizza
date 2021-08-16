d529bd01592f959580161016b478b90dee253b6a
Carlos Eduardo Angulo Ustarez <carlos_angulo_az@hotmail.com>
2021-08-15 22:08:33 -0400

    Call to GetPizzas REST API Service

---
+++ app/screens/config.js
@@ -0,0 +1,3 @@
+module.exports = global.config = {
+  url: "http://192.168.0.13/GreatPizza.API/main/",
+};
--- app/screens/pizzas.js
+++ app/screens/pizzas.js
@@ -1,55 +1,75 @@
-import * as React from "react";
-import { View, Text } from "react-native";
+import React, { useState, useEffect } from "react";
+import { StyleSheet, View, Text, ActivityIndicator } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { ListItem, Avatar, Button, Icon } from "react-native-elements";
 
 export default function Pizzas({ navigation }) {
-  const list = [
-    {
-      pizzaid: 1,
-      description: "Hawaiian",
-      subtitle: "Pizza",
+  const [load, setLoad] = useState();
+  const [pizzas, setPizzas] = useState({});
+
+  useEffect(() => {
+    loadData();
+  }, []);
+
+  const loadData = async () => {
+    setLoad(true);
+    fetch("http://192.168.0.13/GreatPizza.API/main/GetPizzas/")
+      .then((response) => response.json())
+      .then(function (data) {
+        setPizzas(data.pizzas);
+        setLoad(false);
+        console.log(data);
+      });
+  };
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: "center",
     },
-    {
-      pizzaid: 2,
-      description: "Peperoni",
-      subtitle: "Pizza",
+    horizontal: {
+      flexDirection: "row",
+      justifyContent: "space-around",
+      padding: 10,
     },
-    {
-      pizzaid: 3,
-      description: "Irish",
-      subtitle: "Pizza",
-    },
-  ];
+  });
 
   return (
     <View>
-      {list.map((l, i) => (
-        <ListItem
-          key={l.pizzaid}
-          bottomDivider
-          onPress={() => console.log({ i })}
-        >
-          <Icon name="pizza-slice" type="font-awesome-5" color="#f81" />
-          <ListItem.Content>
-            <ListItem.Title>{l.description}</ListItem.Title>
-            <ListItem.Subtitle>{l.subtitle}</ListItem.Subtitle>
-          </ListItem.Content>
-          <Icon
-            raised
-            name="trash"
-            type="font-awesome-5"
-            color="#f50"
-            onPress={() => console.log("hello")}
-          />
-        </ListItem>
-      ))}
+      {load ? (
+        <View style={[styles.container, styles.horizontal]}>
+          <ActivityIndicator size="large" color="#0000ff" />
+        </View>
+      ) : (
+        <View>
+          {pizzas.map((l, i) => (
+            <ListItem
+              key={l.pizzaid}
+              bottomDivider
+              onPress={() => console.log({ i })}
+            >
+              <Icon name="pizza-slice" type="font-awesome-5" color="#f81" />
+              <ListItem.Content>
+                <ListItem.Title>{l.description}</ListItem.Title>
+                <ListItem.Subtitle>{l.subtitle}</ListItem.Subtitle>
+              </ListItem.Content>
+              <Icon
+                raised
+                name="trash"
+                type="font-awesome-5"
+                color="#f50"
+                onPress={() => console.log("hello")}
+              />
+            </ListItem>
+          ))}
 
-      <Button
-        title="Add new Pizza"
-        onPress={() => navigation.navigate("PizzaDetail")}
-      />
+          <Button
+            title="Add new Pizza"
+            onPress={() => navigation.navigate("PizzaDetail")}
+          />
+        </View>
+      )}
     </View>
   );
 }
